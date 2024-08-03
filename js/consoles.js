15640210126e5e95977e315ce9cfe1cea3dd47ec
mgnvr <sergmiren@yandex.ru>
2024-08-03 18:17:10 +0500

    update consoles page

---
+++ js/consoles.js
@@ -0,0 +1,27 @@
+const swiperXbox = new Swiper(".swiper--xbox", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination--xbox",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next--xbox",
+    prevEl: ".swiper-button-prev--xbox",
+  },
+});
+
+const swiperSwitch = new Swiper(".swiper--switch", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination--switch",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next--switch",
+    prevEl: ".swiper-button-prev--switch",
+  },
+});
