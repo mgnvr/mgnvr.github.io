889accdaa6d48533e5304e98de53b7e5ddd4b17a
mgnvr <sergmiren@yandex.ru>
2024-10-21 15:04:19 +0500

    update landing

--- js/consoles.js
+++ js/consoles.js
@@ -25,3 +25,42 @@ const swiperSwitch = new Swiper(".swiper--switch", {
     prevEl: ".swiper-button-prev--switch",
   },
 });
+
+const swiperVideo = new Swiper(".swiper--video", {
+  direction: "horizontal",
+  loop: true,
+  // autoplay: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination--video",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next--video",
+    prevEl: ".swiper-button-prev--video",
+  },
+});
+
+// swiperVideo.on('init', function () {
+//   const activeSlide = swiperVideo.slides[swiperVideo.activeIndex];
+//   const activeVideo = activeSlide.querySelector('video');
+//   if (activeVideo) {
+//     activeVideo.play();
+
+//   }
+// })
+
+
+swiperVideo.on('slideChangeTransitionEnd', function () {
+  swiperVideo.slides.forEach((slide) => {
+    const video = slide.querySelector('video');
+    if (video) {
+      video.pause();
+    }
+  });
+
+  const activeSlide = swiperVideo.slides[swiperVideo.activeIndex];
+  const activeVideo = activeSlide.querySelector('video');
+  if (activeVideo) {
+    activeVideo.play();
+  }
+});
