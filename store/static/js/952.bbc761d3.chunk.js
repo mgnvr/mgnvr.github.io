2a64fcbd594ea28821f839f5ac1a52d5e58bf992
mgnvr <sergmiren@yandex.ru>
2025-04-18 14:41:03 +0500

    add store

---
+++ store/static/js/952.bbc761d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkps_store=self.webpackChunkps_store||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.bbc761d3.chunk.js.map
